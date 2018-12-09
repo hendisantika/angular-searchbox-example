586a19755f4135caac6ff90432a0e428898a6c38
Hendi Santika <hendisantika@gmail.com>
2018-12-10 04:07:23 +0700

    #7 - Modify youtube-search.service.ts file

--- src/app/services/youtube-search.service.ts
+++ src/app/services/youtube-search.service.ts
@@ -1,10 +1,41 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+
+import {Observable} from 'rxjs';
+import {VideoDetail} from '../youtube/video-detail.model';
+
+const YOUTUBE_API_KEY = 'AIzaSyAG0ng-rKp5BxpA-nNC2EG96ilKGMzfA6Y';
+const YOUTUBE_API_URL = 'https://www.googleapis.com/youtube/v3/search';
 
 @Injectable({
   providedIn: 'root'
 })
 export class YoutubeSearchService {
 
-  constructor() {
+  constructor(private http: HttpClient) {
+  }
+
+  search(query: string): Observable<VideoDetail[]> {
+    const params: string = [
+      `q=${query}`,
+      `key=${YOUTUBE_API_KEY}`,
+      `part=snippet`,
+      `type=video`,
+      `maxResults=10`
+    ].join('&');
+
+    const queryUrl = `${YOUTUBE_API_URL}?${params}`;
+
+    return this.http.get(queryUrl).pipe(map(response => {
+      return response['items'].map(item => {
+        return new VideoDetail({
+          id: item.id.videoId,
+          title: item.snippet.title,
+          description: item.snippet.description,
+          thumbnailUrl: item.snippet.thumbnails.high.url
+        });
+      });
+    }));
   }
 }
--- src/app/youtube/video-detail.model.ts
+++ src/app/youtube/video-detail.model.ts
@@ -1,4 +1,4 @@
-export class VideoDetail {
+export class VideoDetailModel {
   id: string;
   title: string;
   description: string;
