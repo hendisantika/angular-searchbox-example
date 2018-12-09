97adcba7064e95cc7d22bfc339a2b404cbce54a8
Hendi Santika <hendisantika@gmail.com>
2018-12-10 03:57:47 +0700

    #4 - ng g c youtube/search-result

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {SearchBoxComponent} from './youtube/search-box/search-box.component';
+import {SearchResultComponent} from './youtube/search-result/search-result.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchBoxComponent
+    SearchBoxComponent,
+    SearchResultComponent
   ],
   imports: [
     BrowserModule
