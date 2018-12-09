22688fb7ff80ea787ad620a8bcd774696ca25a87
Hendi Santika <hendisantika@gmail.com>
2018-12-10 03:58:58 +0700

    #5 - Add Http Client Module in app.module.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
 import {AppComponent} from './app.component';
 import {SearchBoxComponent} from './youtube/search-box/search-box.component';
@@ -12,7 +13,8 @@ import {SearchResultComponent} from './youtube/search-result/search-result.compo
     SearchResultComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
